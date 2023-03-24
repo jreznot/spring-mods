7cdf5d9a7e3b82761d037d909f06f945142a28a2
Yuriy Artamonov <jreznot@gmail.com>
2023-03-24 20:03:05 +0100

    Conditional

--- src/main/java/com/example/demoscan/DemoMod.java
+++ src/main/java/com/example/demoscan/DemoMod.java
@@ -1,11 +1,13 @@
 package com.example.demoscan;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan(
      basePackages = {"com.example.demoscan.mods.hi"}
 )
+@ConditionalOnProperty(name = "mod.hi.enabled", havingValue = "true")
 @Configuration
 public class DemoMod {
 }
